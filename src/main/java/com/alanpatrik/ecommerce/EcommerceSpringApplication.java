7ed8b503c61c55055c041776aece097845f1f648
AlanPatrik <alan.santos@ilabqa.com>
2021-06-29 15:29:30 -0300

    project created

---
+++ src/main/java/com/alanpatrik/ecommerce/EcommerceSpringApplication.java
@@ -0,0 +1,13 @@
+package com.alanpatrik.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alanpatrik/ecommerce/EcommerceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alanpatrik.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
