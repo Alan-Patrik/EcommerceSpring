bfb43b07043174bf27f50c4f2b3b49637410b698
AlanPatrik <alan.santos@ilabqa.com>
2021-06-30 11:00:59 -0300

    adding H2 database, test profile, JPA

--- src/main/java/com/alanpatrik/ecommerce/controller/UserController.java
+++ src/main/java/com/alanpatrik/ecommerce/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.alanpatrik.ecommerce.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +15,22 @@ import com.alanpatrik.ecommerce.entities.User;
 public class UserController {
 	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Alan Patrik", "alanpatrik.fragozo@gmail.com", "2498867-1021", "123456");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = new ArrayList<>();
+		list.add(new User(1L, "Alan Patrik", "alanpatrik.fragozo@gmail.com", "2498867-1021", "123456"));
+		return ResponseEntity.ok().body(list);
 	}
+	
+//	@GetMapping("/{id}")
+//	public ResponseEntity<User> findById(@PathVariable Long id) {
+//		List<User> list = new ArrayList<>();
+//		User user = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+//		if (user == null) {
+//			System.out.println("Id não encontrado!" + user);
+//		}
+//		
+//		return ResponseEntity.ok().body(user);
+//	}
+	
 
 }
