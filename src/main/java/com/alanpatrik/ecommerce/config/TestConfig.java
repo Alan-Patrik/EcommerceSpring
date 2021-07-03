17847d850a88323bb177eb88b144dec11a3788e3
AlanPatrik <alan.santos@ilabqa.com>
2021-07-03 15:34:29 -0300

    Payment, one-to-one association

--- src/main/java/com/alanpatrik/ecommerce/config/TestConfig.java
+++ src/main/java/com/alanpatrik/ecommerce/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.alanpatrik.ecommerce.entities.Category;
 import com.alanpatrik.ecommerce.entities.Order;
 import com.alanpatrik.ecommerce.entities.OrderItem;
+import com.alanpatrik.ecommerce.entities.Payment;
 import com.alanpatrik.ecommerce.entities.Product;
 import com.alanpatrik.ecommerce.entities.User;
 import com.alanpatrik.ecommerce.entities.enums.OrderStatus;
@@ -82,5 +83,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
